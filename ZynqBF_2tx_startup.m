abbe529279d8945cddd56055b5ad08f23fd0e15a
meyer <meyer@DESKTOP-CF54M0S>
2019-01-22 21:03:37 -0500

    Updated project startup script to run with Windows machine.

--- ZynqBF_2tx_startup.m
+++ ZynqBF_2tx_startup.m
@@ -1,1 +1,5 @@
-hdlsetuptoolpath('Toolname', 'Xilinx Vivado', 'Toolpath', '/opt/Xilinx/Vivado/2017.4/bin/vivado')
+if strcmp(computer, 'PCWIN64')
+    hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','C:\Xilinx\Vivado\2017.4\bin')
+else
+    hdlsetuptoolpath('Toolname', 'Xilinx Vivado', 'Toolpath', '/opt/Xilinx/Vivado/2017.4/bin/vivado')
+end
